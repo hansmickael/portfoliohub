71685d03a1fe5738f4aeff0f3708dcb7d1aa252f
hansmickael <161663833+hansmickael@users.noreply.github.com>
2024-06-23 19:21:22 +0800

    Update check.php

--- admin/php/check.php
+++ admin/php/check.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 if(isset($_SESSION['username'])){
-    header('location:../');
+    header('location:../index.php');
 }
 include("../../include/db.php");
 if(isset($_POST['login'])){
