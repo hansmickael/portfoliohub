e0ea5b538b914588deaa0b3b434576e3ef2c102e
hansmickael <161663833+hansmickael@users.noreply.github.com>
2024-06-23 19:08:07 +0800

    Update check.php

--- admin/php/check.php
+++ admin/php/check.php
@@ -5,11 +5,11 @@ if(isset($_SESSION['username'])){
 }
 include("../../include/db.php");
 if(isset($_POST['login'])){
-    $email = mysqli_real_escape_string($db,$_POST['email']);
-    $password = mysqli_real_escape_string($db,$_POST['password']);
+    $email = mysqli_real_escape_string($conn,$_POST['email']);
+    $password = mysqli_real_escape_string($conn,$_POST['password']);
     
     $query="SELECT * FROM admin_users WHERE user_id='$email' AND user_pass='$password'";
-    $run = mysqli_query($db,$query);
+    $run = mysqli_query($conn,$query);
     $result = mysqli_fetch_array($run);
     if($result){
         $_SESSION['id']=$result['id'];
